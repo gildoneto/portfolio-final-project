9bf4375f99279118c48192eaee961510df836736
Gildo Neto <gildo.neto@thoughtworks.com>
2023-04-15 23:45:19 -0300

    finaliza preloader

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,17 @@
+// variables
 const btnContact = document.querySelector('.gn-btn-contact');
 
+// page preloader
+window.addEventListener('load', () => {
+  const pagePreloader = document.querySelector('.gn-preloader');
+  pagePreloader.classList.add('gn-fade-out');
+
+  setTimeout(() => {
+    pagePreloader.style.display = 'none';
+  }, 2000);
+});
+
+// open/close contact info
 btnContact.addEventListener('click', (e) => {
   const boxContact = document.querySelector('.gn-contact-info');
   boxContact.classList.toggle('gn-is-open');
