2cc9309698669b9a90f536cdb193b5317239b855
Gildo Neto <gildo.neto@thoughtworks.com>
2023-04-16 22:21:56 -0300

    modal javascript e animation

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,8 @@
 // variables
 const btnContact = document.querySelector('.gn-btn-contact');
+const toggleModal = document.querySelectorAll('.gn-toggle-modal');
+const overlay = document.querySelector('.gn-overlay');
+const modalOrcamento = document.querySelector('#gn-modal-orcamento');
 
 // page preloader
 window.addEventListener('load', () => {
@@ -18,3 +21,12 @@ btnContact.addEventListener('click', (e) => {
   e.target.classList.toggle('gn-change-icon');
 });
 
+// open-close orçamento modal
+
+toggleModal.forEach((element) => {
+  element.addEventListener('click', () => {
+    overlay.classList.toggle('gn-is-open');
+    modalOrcamento.classList.toggle('gn-is-open');
+    modalOrcamento.classList.toggle('gn-slide-top-in');
+  });
+});
